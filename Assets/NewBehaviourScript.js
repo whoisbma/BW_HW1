96d2692cc15bf269bde70d09dd3d2adc6a4a74bc
Bryan Ma <bryanhma@gmail.com>
2014-02-02 23:08:49 -0500

    first commit

---
+++ Assets/NewBehaviourScript.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var moveTime = 100;
+var moveDir = 1;
+	
+function Start () {
+
+}
+
+function Update () {
+	if (moveTime > 0) {
+		if (moveDir > 0) {
+			transform.Translate(Vector3.up * Time.deltaTime/5, Space.World);
+			transform.Translate(Vector3.forward * Time.deltaTime/4, Space.World);
+		}
+		else {
+			transform.Translate(Vector3.down * Time.deltaTime/5, Space.World);
+			transform.Translate(Vector3.back * Time.deltaTime/4, Space.World);
+		}
+	}
+	else {
+		moveDir = -moveDir;
+		moveTime = 100;
+	}
+	moveTime--;
+}
